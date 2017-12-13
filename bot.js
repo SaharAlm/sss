2f05b3dd3438348f9afd9fa446b505072a87bdcf
Twins1 <33881658+Twins1@users.noreply.github.com>
2017-12-13 23:29:08 +0200

    yo4

--- bot.js
+++ bot.js
@@ -6,6 +6,19 @@ const prefix = ".";
 var bot = new Discord.Client();
 
 bot.on('guildMemberAdd', member => {
+  if (member.guild.id === '384420942010515466') {
+    member.setNickname(`??${member.user.username}`)
+    member.addRole('384420942010515466', 'Joined in the 90\'s')
+  }
+    member.guild.channels.get('353436192353681411').send('**' + member.user.username + '**, Has Joined!')
+});
+
+bot.on("guildMemberRemove", member => {
+    member.guild.channels.get('353436192353681411').send('**' + member.user.username + '**, ByeBye')
+
+});
+
+bot.on('guildMemberAdd', member => {
   member.guild.channels.get('346023639516315649').send(`**${member.user.username}** has Joined!`)
 });
 

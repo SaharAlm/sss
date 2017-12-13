a5d28ea51904f33e553c0e9afe2981e2ef030e66
Twins1 <33881658+Twins1@users.noreply.github.com>
2017-12-13 19:13:41 +0200

    Initial commit

---
+++ bot.js
@@ -0,0 +1,83 @@
+const Discord = require("discord.js");
+
+const TOKEN = "MzgxODgxMDgyODQyMTg1NzQw.DPrONA.pixPZKkbdr_wYXQn7mg_AKXSdn0";
+const prefix = ".";
+
+bot.on('guildMemberAdd', member => {
+  member.guild.channels.get('346023639516315649').send(`**${member.user.username}** has Joined!`)
+});
+
+bot.on('guildMemberRemove', member => {
+  member.guild.channels.get('346023639516315649').send(`**${member.user.username}** has left... ByeBye`)
+});
+
+var fortunes = [ // this is gonna be the random place
+   "yes",
+   "no",
+   "fucc",
+   "u gay M8",
+   "HOMO",
+   "i dont agree bitch",
+   "what u gonna do?"
+];
+var bot = new Discord.Client();
+
+bot.on("ready", function() {
+  console.log("bobo is in the house");
+})
+
+bot.on ("message", function(message) {
+  if (message.author.equals(bot.user)) return;
+
+  if (!message.content.startsWith(prefix));
+
+  var args = message.content.substring(prefix.length).split(" ");
+
+  switch (args[0].toLowerCase()) {
+    case "ping":
+        message.channel.send("Pong!");
+        break;
+    case "info":
+        message.channel.send("I am mister BoBo and, im a Twin!")
+        break;
+    case "8ball":
+        if (args[1]) message.channel.send(fortunes[Math.floor(Math.random() * fortunes.length)]);
+        else message.channel.send("cant read that!")
+        break;
+    case "סני":
+        message.channel.send("ילד רע")
+        break;
+    case "pussy":
+        message.channel.send("סני הוא סוג של ניגה שמפחד להיות ניבא")
+        break;
+    case "mlgsquad":
+        message.channel.send("This is my squad Biatch!")
+        break;
+    case "creator!":
+        message.channel.send("so.... my name is sahar im 13 YearsOld and i am love to be dank")
+        break;
+    case "eli":
+        message.channel.send("ran him ha katan");
+        break;
+    case "faggot":
+        message.channel.send("למי קראת פאג אה ביץ")
+        break;
+    case "hate&seny":
+        message.channel.send("give 2 seny a lot of hate/love idk...")
+        break;
+    case "seny'sinfo":
+        message.channel.send("בשרת של סני אתם עומדים להנות בקלל!")
+        break;
+    case "invite2":
+        message.channel.send("הנה קישור לשרת של סני https://discord.gg/JqE9Paq ")
+        break;
+    case "rules":
+        message.reply("``` חוקים: לא לקלל, לא לא להספים, לא להשתמש באימוגים, לא להיות טרולרים, להנות הכי חשוב   ```")
+        break;
+    case "senycm":
+        message.reply("```.rules                                                                                                                                                .סני                                                                                                                                                .seny'sinfo                                                                                                                                                .8ball                                                                                                                                                .senycm ``` ")
+        break;
+  }
+});
+
+bot.login(TOKEN);
